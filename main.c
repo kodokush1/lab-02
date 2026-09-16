bbd54f0b972b141e3ec0b07d02e963370f7b0907
Yurovskiy Pavel <pavelpavlovi3h@gmail.com>
2026-09-16 10:41:15 +0600

    Второй коммит: добавлена возможность обнуления инвентаря, отображение инвентаря, меню

--- main.c
+++ main.c
@@ -3,48 +3,60 @@
 
 int current_day = 1;
 int current_hour = 8;
-int inventory = {10};
+const char* inventory[10] = {"Пусто", "Дерево", "Камень", "Семена", "Дерево", "Железо", "Кирка", "Ведро", "Дерево", "Мотыга"};
 int skip_time = 0;
+const char* slot = " ";
+int i = 0;
+int id_slot = 0;
 
-int main(){
+int main(void){
     
     while(1){
         
-        int i = 0;
-        scanf("%d", &i);
+        printf("--------МЕНЮ--------\n1.Текущее время\n2.Пропуск времени\n3.Показать инвентарь\n4.Поднять предмет\n5.Обнуление слота\n6.Любимый ресурс\n0.Завершить программу\n");
         
-        switch(i)
+        int number = 0;
+        scanf("%d", &number);
+        
+        switch(number)
         {
             case 1 :
+                
                 printf("Текущее время: День: %d, %d:00\n", current_day, current_hour);
                 break;
+                
             case 2 :
-                printf("Ск2олько часов вы хотите пропустить? ");
+                
+                printf("Сколько часов вы хотите пропустить? ");
                 scanf("%d", &skip_time);
                 current_hour = current_hour + skip_time;
                 while (current_hour >= 24) {current_hour = current_hour - 24; current_day += 1;}
                 skip_time = 0;
                 break;
+                
             case 3 :
+                
+                for(int i = 0; i < 10; ++i){
+                    printf("Слот[%d]: %s \n", i, inventory[i]);
+            
+                }
                 break;
             case 4 :
-                printf("Текущее время: День: %d, %d:00\n", current_day, current_hour);
+
                 break;
+                
             case 5 :
-                printf("Текущее время: День: %d, %d:00\n", current_day, current_hour);
+                
+                printf("Введите номер слота, который хотите обнулить\n");
+                scanf("%d", &id_slot);
+                inventory[id_slot] = "Пусто";
+                id_slot = 0;
                 break;
+                
             case 6 :
-                printf("Текущее время: День: %d, %d:00\n", current_day, current_hour);
+                printf("Задание по варианту\n");
                 break;
-            case 7 :
-                printf("Текущее время: День: %d, %d:00\n", current_day, current_hour);
-                break;
-            case 8 :
-                printf("Текущее время: День: %d, %d:00\n", current_day, current_hour);
-                break;
-            case 9 :
-                printf("Текущее время: День: %d, %d:00\n", current_day, current_hour);
-                break;
+
             case 0 :
                 return 0;
 
